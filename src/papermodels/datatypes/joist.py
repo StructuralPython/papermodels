a4a4fda7dea150963360ebb7d230a03053f380c5
Connor Ferster <38441489+connorferster@users.noreply.github.com>
2023-11-08 12:32:17 -0800

    Added: joist.py; updated: pyproject.toml
    - Added pycba dependency

--- src/papermodels/datatypes/joist.py
+++ src/papermodels/datatypes/joist.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Optional
+import pycba as cba
+
 
 @dataclass
 class Joist:
@@ -17,24 +19,43 @@ class Joist:
     span: float | Any
     a: float | Any = 0.
     b: float | Any = 0.
+    
+    def __post_init__(self):
+        L=[self.a, self.span, self.b]
+        EI=[1e3, 1e3, 1e3]
+        R=[0., 0., -1., 0., -1., 0., 0., 0.,]
+
+        if self.a == 0:
+            L.pop(0)
+            EI.pop(0)
+            R.pop(0)
+            R.pop(0)
+        if self.b == 0:
+            L.pop()
+            EI.pop()
+            R.pop()
+            R.pop()
+
+        self._pycba_model = cba.BeamAnalysis(
+            L,
+            EI,
+            R,
+        )
+        for idx, _ in enumerate(L):
+            self._pycba_model.add_udl(idx + 1, 1) # 1-based idx
+
 
     def get_r1(self):
-        w = 1
-        r1_a = w * self.a / (2 * self.span) * (2 * self.span + self.a)
-        r1_span = w * self.span / 2
-        r1_b = -w * self.a**2 / (2 * self.span)
-        total_r1 = sum([r1_a, r1_span, r1_b])
+        self._pycba_model.analyze()
+        total_r1 = self._pycba_model._beam_results.R[0]
         total_load = self.get_total_load()
-        return total_r1 / total_load
+        return round(total_r1 / total_load, 9)
     
     def get_r2(self):
-        w = 1
-        r2_b = w * self.b / (2 * self.span) * (2 * self.span + self.b)
-        r2_span = w * self.span / 2
-        r2_a = -w * self.b**2 / (2 * self.span)
-        total_r2 = sum([r2_a, r2_span, r2_b])
+        self._pycba_model.analyze()
+        total_r2 = self._pycba_model._beam_results.R[1]
         total_load = self.get_total_load()
-        return total_r2 / total_load
+        return round(total_r2 / total_load, 9)
 
     def get_total_load(self):
         w = 1
