9a1c87f0d31dc23f8057552b5da84f1597b5ad4e
Connor Ferster <connorferster@gmail.com>
2024-04-01 00:20:56 -0700

    chore: update imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,6 @@
 def test_imports():
     from papermodels.datatypes.annotation import Annotation
-    from papermodels.models import load_distribution
+    from papermodels.loads import load_distribution
     from papermodels.paper import annotations
     from papermodels.paper import fdf
     from papermodels.paper import plot
