396d136198c7f2312bc1e0baf777e9e678523cd6
Connor Ferster <connorferster@gmail.com>
2025-09-03 10:44:54 -0700

    tests: added more rigorous tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -80,5 +80,18 @@ def test_collector_extent_loads(load_collector_extents):
 
 def test_collector_extent_creates_loaded_elements(collector_extents_to_trib_loaded_elements):
     les = collector_extents_to_trib_loaded_elements
-    assert list(les.keys()) == ['CT0.1', 'SJ0.0-0', 'SJ0.0-1', 'SJ0.0-2', 'SJ0.0-3', 'SJ0.0-4', 'SJ0.0-5', 'SJ0.0-6', 'SJ0.1', 'WT0.3', 'FB0.0', 'FB0.2', 'FB0.1', 'FB0.3', 'WT0.1', 'CT0.4', 'CT0.5', 'CT0.3', 'CT0.8', 'WT0.0', 'CT0.0', 'CT0.6', 'WT0.2', 'CT0.2', 'CT0.7']
-    assert les['WT0.3'].model()['loads']['distributed_loads']
+    assert set(les.keys()) == set(['SJ0.0-0', 'SJ0.0-1', 'SJ0.0-2', 'SJ0.0-3', 'SJ0.0-4', 'SJ0.0-5', 'SJ0.0-6', 'SJ0.1', 'WT0.0', 'WT0.1', 'WT0.2', 'WT0.3', 'FB0.0', 'FB0.2', 'FB0.1', 'FB0.3', 'CT0.4', 'CT0.5', 'CT0.8', 'FB0.4', 'WT0.0', 'CT0.0', 'CT0.6', 'WT0.2', 'CT0.2', 'CT0.7', 'WT0.1', 'CT0.1', 'CT0.3'])
+    assert les['WT0.3'].model()['loads']['distributed_loads'] # There are loads present on the intermediate support
+
+    assert les['WT0.1'].model()['loads']['distributed_loads'][0]['transfer_source'] == "SJ0.0-0"
+    assert les['WT0.1'].model()['loads']['distributed_loads'][0]['start_loc'] == 2.521
+    assert les['WT0.1'].model()['loads']['distributed_loads'][0]['end_loc'] == 3.858
+
+    # This member experiences a splitting that occurs from an intermediate support that is found
+    # within its overlap region.
+    assert les['FB0.0'].model()['loads']['distributed_loads'][0]['transfer_source'] == "SJ0.0-2"
+    assert les['FB0.0'].model()['loads']['distributed_loads'][0]['start_loc'] == 0.326
+    assert les['FB0.0'].model()['loads']['distributed_loads'][0]['end_loc'] == 3.01
+    assert les['FB0.0'].model()['loads']['distributed_loads'][1]['transfer_source'] == "SJ0.0-3"
+    assert les['FB0.0'].model()['loads']['distributed_loads'][1]['start_loc'] == 3.01
+    assert les['FB0.0'].model()['loads']['distributed_loads'][1]['end_loc'] == 3.912
